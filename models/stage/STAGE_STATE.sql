85ae50ea4ce9eb9c59185dd8331784604c1ac5a5
Michael Kubacki <mikeartz@hotmail.com>
2025-09-23 15:00:48 +0000

    Updated schema

--- models/stage/STAGE_STATE.sql
+++ models/stage/STAGE_STATE.sql
@@ -2,7 +2,5 @@
 
 {{ config(materialized='table') }}
 
-select distinct 
-    State_Name,
-    State_Abbreviation
-from {{source('stage_covid','stage_state')}}
+select STATE AS state_name,state_abbreviation 
+FROM STAGE_COVID.STAGE.STAGE_STATE;
